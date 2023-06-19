981a96fe264385a2357f28bd3bb3b31709507548
RafaelRMJesus <rafaelrocha191919@gmail.com>
2023-06-19 17:01:53 -0300

    animations

--- js/app.js
+++ js/app.js
@@ -7,13 +7,56 @@ window.addEventListener('mousemove', (e) => {
   let xValue = e.clientX - window.innerWidth / 2;
   let yValue = e.clientY - window.innerHeight / 2;
 
-  let zValue = 100;
+
 
   parallax_el.forEach((element) => {
 
     let speedx = element.dataset.speedx;
     let speedy = element.dataset.speedy;
 
-    element.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px)) perspective(2300px) translateZ(${zValue}px)`;
+    element.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
   });
 });
+
+let timeline = gsap.timeline();
+
+Array.from(parallax_el)
+  .filter(el => !el.classList.contains("text"))
+  .forEach((el) => {
+    timeline.from(
+      el,
+      {
+      top: `${el.offsetHeight / 2 + +el.dataset.distance}px`,
+      duration: 3.5,
+      ease: "power3.out"
+  },
+  "1"
+  )
+});
+
+timeline.from(".text .teste h1", {
+  y: window.innerHeight - document.querySelector(".text h1").getBoundingClientRect().top,
+  duration: 2,
+},
+  "2.5"
+).from(".text h2", {
+  y: -150, 
+  opacity: 0,
+  duration: 1.5,
+}, "3")
+
+timeline.from(".text .teste p", {
+  y: window.innerHeight - document.querySelector(".text p").getBoundingClientRect().top,
+  duration: 2,
+},
+  "2.5"
+)
+
+timeline.from(".text .teste i", {
+  y: window.innerHeight - document.querySelector(".text i").getBoundingClientRect().top,
+  duration: 2,
+},
+  "2.5"
+)
+
+
